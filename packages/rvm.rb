01370185932cdd0e7358ee70e424954be3a1461d
Joe Ferris <jferris@thoughtbot.com>
2011-03-28 11:24:01 -0400

    Only install rvm once

--- packages/rvm.rb
+++ packages/rvm.rb
@@ -4,12 +4,18 @@ package :rvm do
     post :install, %{sudo -u jenkins -H bash /tmp/rvm_config}
   end
   requires :bashrc, :curl
+  verify do
+    has_directory "/var/lib/jenkins/.rvm"
+  end
 end
 
 package :bashrc do
   description "~/.bashrc for rvm"
   transfer "assets/bashrc", "/tmp" do
     post :install, %{mv /tmp/bashrc /var/lib/jenkins/.bashrc}
+  end
+  verify do
+    file_contains "/var/lib/jenkins/.bashrc", "rvm"
   end
 end
 
